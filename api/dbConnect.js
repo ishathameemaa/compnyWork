942adab7d57f023c48508a2ded9890feeb9370b9
ishathameemaa <ishathameemaa@gmail.com>
2025-04-11 22:09:27 +0530

    first commit

---
+++ api/dbConnect.js
@@ -0,0 +1,12 @@
+import mongoose from 'mongoose';
+
+const dbConnect = async () => {
+    if (mongoose.connections[0].readyState) {
+        console.log("Already Connected");
+        return;
+    }
+    await mongoose.connect(process.env.MONGODB_URI);
+    console.log("DB Connected");
+};
+
+export default dbConnect;
